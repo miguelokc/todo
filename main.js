74d0bc6e4832e58fd5077e49089909fe48fd897a
miguelokc <185961450+miguelokc@users.noreply.github.com>
2025-06-10 14:56:40 -0500

    added checkbox and edit button

--- main.js
+++ main.js
@@ -5,17 +5,26 @@ const tasks = document.getElementById('tasks');
 
 button.addEventListener('click',()=>{
     const btn = document.createElement('button');
+    
     const text = bar.innerText.trim();
     if (text !== ""){
         const task = document.createElement('div');
+        const span = document.createElement('span');
+        const edit = document.createElement('button');
+        span.innerText = text;
+        const checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        task.appendChild(checkbox);
+        task.appendChild(span);
         task.className = 'task';
-        task.innerText = text;
         tasks.appendChild(task);
         btn.innerText = 'delete';
         task.appendChild(btn);
         btn.addEventListener('click',()=>{
             task.remove();
         })
+        edit.innerText = 'Edit';
+        task.appendChild(edit);
         bar.innerText = "";
     }
 });
